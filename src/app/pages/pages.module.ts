8565ec60da9db06b11f33d748d4596b53a1ff01f
fjl2020 <fjl2020@gmail.com>
2022-10-13 20:26:39 +0200

    Fin sección 4

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterOutlet} from '@angular/router'
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ShareModule } from '../share/share.module';
+import { PagesRoutingModule } from './pages-routing.module';
+// import { AppRoutingModule } from '../app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+
+    
+  ],
+  imports: [
+    CommonModule,
+    ShareModule,
+    PagesRoutingModule,
+    RouterOutlet
+
+
+  ],
+  exports:[
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+  ]
+})
+export class PagesModule { }
